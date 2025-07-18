5b2d395004c5e3b63e18ce7fd9f78007d0153428
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-18 18:00:54 +0200

    [add] feat add li with mail

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+const list = document.getElementById('mail-list');
+
 for (let i = 0; i < 10; i++) {
   axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
     console.log(resp.data.response);
+    list.innerHTML += `<li>${resp.data.response}</li>`
   })
 }
 
